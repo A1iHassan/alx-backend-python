798b40ee9ba8d24d3a8628e2371695abb837db51
A1iHassan <ali012wkout@gmail.com>
2024-11-21 23:17:01 +0200

    a commit by Ali Hassan

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python3
+"""
+Type checking
+Use mypy to validate code piece
+apply any necessary changes
+"""
+from typing import Tuple, List
 
-from typing import Tuple, Any, Union, Sequence,List
 
-
-def zoom_array(lst: Sequence[int], factor: Union[int, float] = 2) -> Tuple[Union[int, float]]:
-
-    if isinstance(factor, float):
-        factor = int(factor)
-    zoomed_in: List[Union[int, float]]= [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Return necessary changes
+    """
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
-    ]
+        ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array: Tuple = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
